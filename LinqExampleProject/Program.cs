65d8adaa7560a561c3f6fdf2507caef51fe85685
ALİ KARA <77415061+aliikara@users.noreply.github.com>
2021-02-02 21:04:01 +0300

    Proje dosyası ekle.

---
+++ LinqExampleProject/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryId=1,CategoryName="Bilgisayar"},
+                new Category{CategoryId=2,CategoryName="Telefon"},
+            };
+
+            List<Product> products = new List<Product>
+            {
+                new Product{ProductId=1,CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit="32 GB RAM" ,UnitPrice=10000,UnitInStock=5},
+                new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 GB RAM", UnitPrice=18000,UnitInStock=3},
+                new Product{ProductId=3,CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit="8 GB RAM" ,UnitPrice=18000,UnitInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit="4 GB RAM", UnitPrice=5000,UnitInStock=15},
+                new Product{ProductId=5,CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="4 GB RAM", UnitPrice=8000,UnitInStock=0},
+            };
+
+
+            //METHODLARI ÇALIŞTIRMAK İÇİN AŞAĞIDA BULUNAN AÇIKLAMA SATIRLARININ BAŞINDAKİ "//" SİLEREK ÇALIŞTIRABİLİRSİNİZ.
+
+            //Test(products);
+            //GetProducts(products);
+            //AnyTest(products);
+            //Lambda
+            //FindTest(products);
+            //FindAllTest(products);
+            //AscDescTest(products);
+            //ClassicLinqTest(products);
+            //JoinLİnqTest(categories, products);
+            ForeachLambdaTest(categories);
+
+        }
+
+        private static void ForeachLambdaTest(List<Category> categories) //FOREACH İLE LİSTELEME İŞLEMİ
+        {
+            categories.ForEach(c =>
+            {
+                Console.WriteLine(c.CategoryName);
+            });
+        }
+
+        private static void JoinLİnqTest(List<Category> categories, List<Product> products) //Join Kullanımı
+        {
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         where p.UnitPrice > 5000
+                         orderby p.UnitPrice descending
+                         select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+            foreach (var productDto in result)
+            {
+                Console.WriteLine("{0} ---- {1}", productDto.ProductName, productDto.CategoryName);
+            }
+        }
+
+        private static void ClassicLinqTest(List<Product> products) // Standart LINQ Kullanımı
+        {
+            var result = from p in products
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select new ProductDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void AscDescTest(List<Product> products) // Ürünün İsmine ve Fiyatına Göre Artan-Azalan veya Azalan-Artan Sıralaması
+        {
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products) //Ürün İsminin İçinde "top" Bulunan Ürünleri Listeler
+        {
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            Console.WriteLine(result);
+        }
+
+        private static void FindTest(List<Product> products) // ProductID'si 3 Olan Ürünü Ekrana Yazdırır.
+        {
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.ProductName);
+        }
+
+        private static void AnyTest(List<Product> products) // ELEMAN VAR MI YOK MU DİYE KONTROL SAĞLIYOR
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop");
+            Console.WriteLine(result);
+        }
+
+        private static void Test(List<Product> products) //LINQ Kullanışlı Oluşunu Gösteriyor
+        {
+            Console.WriteLine("Algoritmik --------------------");
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitInStock > 3)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+                //Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("Linq --------------------------");
+
+            var result = products.Where(p => p.UnitPrice > 5000 && p.UnitInStock > 3);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        static List<Product> GetProducts(List<Product> products) //LINQ Kullanışlı Oluşunu Gösteriyor
+        {
+            List<Product> filteredProducts = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitInStock > 3)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+            return filteredProducts;
+        }
+        static List<Product> GetProductsLinq(List<Product> products) //LINQ Kullanışlı Oluşunu Gösteriyor
+        {
+            return products.Where(p => p.UnitPrice > 5000 && p.UnitInStock > 3).ToList();
+        }
+    }
+
+    class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+    }
+
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
